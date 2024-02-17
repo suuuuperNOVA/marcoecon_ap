dddc74ea0b7235622f10bc2a0ebd7530edf6b117
suuuuperNOVA <jinglu_cui@163.com>
2024-02-17 01:20:34 -0800

    Add unit37

--- note_book/_build/jupyter_execute/py/unit038.py
+++ note_book/_build/jupyter_execute/py/unit038.py
@@ -15,10 +15,44 @@
 # : Decrease government spending and/or increase tax collections in order to cool off the economy and lower the price level.
 # : Shift AD to the left.
 
+# ## Long-Run Adjustment to a Recession Gap
+
+# Without the fiscal policy, LRAS can correct the recessionary gap in the **long run** as shown in the figure below:
+# - When the economy is operating at E1, the unemployment rate is high. So, the unemployed workers and idle resources put downward pressure on wages and other production costs.
+# - When the resource prices fall, firms will increase the production, shifting SRAS to the right.
+# - The price level will fall and real GDP will increase.
+# - The economy will operate at a point on LRAS.
+
+# <img src="../pic/unit038_lrsadj.png" alt="lrsadj" width="800"/>
+
+# ## Long-Run Adjustment to a Inflationary Gap
+
+# Without the fiscal policy, LRAS can correct the inflationary gap in the **long run** as shown in the figure below:
+# - When the economy is operating at E1, the unemployment rate is low. So, shortage in labor forces and resources put upward pressure on wages and other production costs.
+# - When the resource prices increase, firms will decrease the production, shifting SRAS to the left.
+# - The price level will increase and real GDP will decrease.
+# - The economy will operate at a point on LRAS.
+
+# <img src="../pic/unit038_lradjinf.png" alt="inf" width="800"/>
+
 # ## Expansionary Fiscal Policy
 # 
+# The government increases spending or reduces tax collections, shifting AD to the right. The economy will operate at the point on LRAS. 
+# 
+# However, an expansionary fiscal policy has drawbacks:
+# - Cause inflation;
+# - Cause budget deficit.
+
+# <img src="../pic/unit038_expansion.png" alt="ex" width="400"/>
+
+# ## Contractionary Fiscal Policy
+# 
 # 
 
-# 
+# The government decreases spending or increases tax collections, shifting AD to the left. The economy will operate at the point on LRAS. The government budget will tend toward a surplus.
 
-# 
+# <img src="../pic/unit038_contract.png" alt="contractionary" width="400"/>
+
+# ## Fiscal Policy in Practice
+
+# There are lags to discretionary fiscal policy because of factors such as the time it takes to decide on and implement a policy action.
--- note_book/_build/jupyter_execute/py/unit039.py
+++ note_book/_build/jupyter_execute/py/unit039.py
@@ -3,6 +3,12 @@
 
 # # Automatic Stabilizers
 
+# Automatic stabilizer
+# : Government policy in place promotes deficit spending during recessions and surplus budgets during expansions to prevent business cycles from becoming too extreme.
+# : Prevent recessions from becoming depressions,
+# : Keep inflations from turning into hyperinflations.
+# : e.g. Temporary Aid to Needy Families.
+
 # In[ ]:
 
 
